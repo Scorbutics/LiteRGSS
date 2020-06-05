714c70755913a2b99dd957d407468a5caf96fdde
Brice BULGARELLI <brice.bulgarelli.prestataire@sfr.com>
2020-06-05 11:07:29 +0200

    feature : use LiteCGSS SpriteMap

--- ext/LiteRGSS/SpriteMap.cpp
+++ ext/LiteRGSS/SpriteMap.cpp
@@ -1,9 +1,8 @@
 #include "LiteRGSS.h"
 #include "SpriteMap.h"
+
 #include "rbAdapter.h"
-#include "CSpriteMap_Element.h"
 #include "Texture_Bitmap.h"
-#include "CRect_Element.h"
 #include "Sprite.h"
 #include "Drawable_Disposable.h"
 #include "Rect.h"
@@ -12,218 +11,174 @@
 VALUE rb_cSpriteMap = Qnil;
 
 template<>
-void rb::Mark<CSpriteMap_Element>(CSpriteMap_Element* sprite)
-{
-	if(sprite == nullptr)
+void rb::Mark<SpriteMapElement>(SpriteMapElement* sprite) {
+	if (sprite == nullptr) {
 		return;
+	}
 	rb_gc_mark(sprite->rViewport);
 	rb_gc_mark(sprite->rX);
 	rb_gc_mark(sprite->rY);
-	rb_gc_mark(sprite->rZ);
 	rb_gc_mark(sprite->rOX);
 	rb_gc_mark(sprite->rOY);
 	rb_gc_mark(sprite->rScale);
 }
 
-VALUE rb_SpriteMap_Initialize(int argc, VALUE* argv, VALUE self)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
+VALUE rb_SpriteMap_Initialize(int argc, VALUE* argv, VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
 	VALUE viewport, tile_width, tile_count;
 	rb_scan_args(argc, argv, "30", &viewport, &tile_width, &tile_count);
 
-	// Viewport push
-	if (rb_obj_is_kind_of(viewport, rb_cViewport) != Qtrue)
-		rb_raise(rb_eRGSSError, "SpriteMap require viewport to be initialized.");
-	
-	//TODO
-	/*
-	auto &viewport_el = rb::Get<CViewport_Element>(viewport);
-	viewport_el.add(sprite);
-	sprite.rViewport = viewport;
-	*/
-	sprite.rViewport = Qnil;
+	auto &viewport_el = rb::GetSafe<ViewportElement>(viewport, rb_cViewport);
+	viewport_el.initAndAdd(spriteMap);
+	spriteMap.rViewport = viewport;
 
-	// Sprite definition
-	sprite.define_map(NUM2ULONG(tile_width), NUM2ULONG(tile_count));
+	spriteMap->defineMap(NUM2ULONG(tile_width), NUM2ULONG(tile_count));
 	return self;
 }
 
-VALUE rb_SpriteMap_Dispose(VALUE self)
-{
-	return rb::Dispose<CSpriteMap_Element>(self);
+VALUE rb_SpriteMap_Dispose(VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	spriteMap->dispose();
+	return Qnil;
 }
 
-VALUE rb_SpriteMap_Viewport(VALUE self)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	return sprite.rViewport;
+VALUE rb_SpriteMap_Viewport(VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	return spriteMap.rViewport;
 }
 
-VALUE rb_SpriteMap_X(VALUE self)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	return sprite.rX;
+VALUE rb_SpriteMap_X(VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	return spriteMap.rX;
 }
 
-VALUE rb_SpriteMap_SetX(VALUE self, VALUE val)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	sprite.setPosition(NUM2DBL(val), NUM2DBL(sprite.rY));
-	sprite.rX = val;
+VALUE rb_SpriteMap_SetX(VALUE self, VALUE val) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	spriteMap->move(NUM2DBL(val), NUM2DBL(spriteMap.rY));
+	spriteMap.rX = val;
 	return self;
 }
 
-VALUE rb_SpriteMap_Y(VALUE self)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	return sprite.rY;
+VALUE rb_SpriteMap_Y(VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	return spriteMap.rY;
 }
 
-VALUE rb_SpriteMap_SetY(VALUE self, VALUE val)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	sprite.setPosition(NUM2DBL(sprite.rX), NUM2DBL(val));
-	sprite.rY = val;
+VALUE rb_SpriteMap_SetY(VALUE self, VALUE val) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	spriteMap->move(NUM2DBL(spriteMap.rX), NUM2DBL(val));
+	spriteMap.rY = val;
 	return self;
 }
 
-VALUE rb_SpriteMap_SetPosition(VALUE self, VALUE x, VALUE y)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	sprite.setPosition(NUM2DBL(x), NUM2DBL(y));
-	sprite.rX = x;
-	sprite.rY = y;
+VALUE rb_SpriteMap_SetPosition(VALUE self, VALUE x, VALUE y) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	spriteMap->move(NUM2DBL(x), NUM2DBL(y));
+	spriteMap.rX = x;
+	spriteMap.rY = y;
 	return self;
 }
 
-VALUE rb_SpriteMap_Z(VALUE self)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	return sprite.rZ;
+VALUE rb_SpriteMap_Z(VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	return LONG2FIX(spriteMap->getZ().z);
 }
 
-VALUE rb_SpriteMap_SetZ(VALUE self, VALUE val)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	NUM2LONG(val);
-	sprite.rZ = val;
+VALUE rb_SpriteMap_SetZ(VALUE self, VALUE val) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	spriteMap->setZ(NUM2LONG(val));
 	return self;
 }
 
-VALUE rb_SpriteMap_OX(VALUE self)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	return sprite.rOX;
+VALUE rb_SpriteMap_OX(VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	return spriteMap.rOX;
 }
 
-VALUE rb_SpriteMap_SetOX(VALUE self, VALUE val)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	sprite.setOrigin(NUM2DBL(val), NUM2DBL(sprite.rOY));
-	sprite.rOX = val;
+VALUE rb_SpriteMap_SetOX(VALUE self, VALUE val) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	spriteMap->moveOrigin(NUM2DBL(val), NUM2DBL(spriteMap.rOY));
+	spriteMap.rOX = val;
 	return self;
 }
 
-VALUE rb_SpriteMap_OY(VALUE self)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	return sprite.rOY;
+VALUE rb_SpriteMap_OY(VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	return spriteMap.rOY;
 }
 
-VALUE rb_SpriteMap_SetOY(VALUE self, VALUE val)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	sprite.setOrigin(NUM2DBL(sprite.rOX), NUM2DBL(val));
-	sprite.rOY = val;
+VALUE rb_SpriteMap_SetOY(VALUE self, VALUE val) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	spriteMap->moveOrigin(NUM2DBL(spriteMap.rOX), NUM2DBL(val));
+	spriteMap.rOY = val;
 	return self;
 }
 
-VALUE rb_SpriteMap_SetOrigin(VALUE self, VALUE x, VALUE y)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	sprite.setOrigin(NUM2DBL(x), NUM2DBL(y));
-	sprite.rOX = x;
-	sprite.rOY = y;
+VALUE rb_SpriteMap_SetOrigin(VALUE self, VALUE x, VALUE y) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	spriteMap->moveOrigin(NUM2DBL(x), NUM2DBL(y));
+	spriteMap.rOX = x;
+	spriteMap.rOY = y;
 	return self;
 }
 
-VALUE rb_SpriteMap_Reset(VALUE self)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	sprite.reset();
+VALUE rb_SpriteMap_Reset(VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	spriteMap->clear();
 	return self;
 }
 
-VALUE rb_SpriteMap_Set(int argc, VALUE* argv, VALUE self)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
+VALUE rb_SpriteMap_Set(int argc, VALUE* argv, VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
 	VALUE index, bitmap, rect;
 	rb_scan_args(argc, argv, "30", &index, &bitmap, &rect);
-	// Retreiving Bitmap
 	auto& bmp = rb::GetSafe<TextureElement>(bitmap, rb_cBitmap);
-	auto& texture = bmp->getTexture();
-	// Retreiving Rect
-	if(rb_obj_is_kind_of(rect, rb_cRect) != Qtrue)
-		rb_raise(rb_eRGSSError, "Expected Rect got %s", RSTRING_PTR(rb_class_name(CLASS_OF(rect))));
-	auto& rect_el = rb::Get<CRect_Element>(rect);
+	auto& rect_el = rb::GetSafe<RectangleElement>(rect, rb_cRect);
 
-	sprite.setTile(NUM2LONG(index), rect_el.getRect(), texture);
-
+	spriteMap->setTile(NUM2LONG(index), rect_el->getRect(), bmp->getTexture());
 	return self;
 }
 
-VALUE rb_SpriteMap_SetRect(int argc, VALUE* argv, VALUE self)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
+VALUE rb_SpriteMap_SetRect(int argc, VALUE* argv, VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
 	VALUE index, x, y, width, height;
 	rb_scan_args(argc, argv, "23", &index, &x, &y, &width, &height);
 
-	if(NIL_P(y)) // We used a rect
-	{
-			if(rb_obj_is_kind_of(x, rb_cRect) != Qtrue)
-				rb_raise(rb_eRGSSError, "Expected Rect got %s", RSTRING_PTR(rb_class_name(CLASS_OF(x))));
-			auto& rect_el = rb::Get<CRect_Element>(x);
-			sprite.setTileRect(NUM2LONG(index), rect_el.getRect());
-	}
-	else
-	{
+	if (NIL_P(y)) {
+		auto& rect_el = rb::GetSafe<RectangleElement>(x, rb_cRect);
+		spriteMap->setTileRect(NUM2LONG(index), rect_el->getRect());
+	} else {
 		sf::IntRect rect = sf::IntRect(NUM2INT(x), NUM2INT(y), NUM2INT(width), NUM2INT(height));
-		sprite.setTileRect(NUM2LONG(index), rect);
+		spriteMap->setTileRect(NUM2LONG(index), rect);
 	}
 	return self;
 }
 
-VALUE rb_SpriteMap_TileScale(VALUE self)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	return sprite.rScale;
+VALUE rb_SpriteMap_TileScale(VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	return spriteMap.rScale;
 }
 
-VALUE rb_SpriteMap_TileScaleSet(VALUE self, VALUE val)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	sprite.setTileScale(NUM2DBL(val));
-	sprite.rScale = val;
+VALUE rb_SpriteMap_TileScaleSet(VALUE self, VALUE val) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	spriteMap->setTileScale(NUM2DBL(val));
+	spriteMap.rScale = val;
 	return self;
 }
 
-VALUE rb_SpriteMap_index(VALUE self)
-{
-	auto& sprite = rb::Get<CSpriteMap_Element>(self);
-	return LONG2NUM(sprite.getDrawPriority());
+VALUE rb_SpriteMap_index(VALUE self) {
+	auto& spriteMap = rb::Get<SpriteMapElement>(self);
+	return LONG2NUM(spriteMap->getZ().index);
 }
 
-
-VALUE rb_SpriteMap_Copy(VALUE self)
-{
+VALUE rb_SpriteMap_Copy(VALUE self) {
 	rb_raise(rb_eRGSSError, "SpriteMaps cannot be cloned or duplicated.");
 	return self;
 }
 
-void Init_SpriteMap()
-{
+void Init_SpriteMap() {
 	rb_cSpriteMap = rb_define_class_under(rb_mLiteRGSS, "SpriteMap", rb_cDrawable);
-	rb_define_alloc_func(rb_cSpriteMap, rb::AllocDrawable<CSpriteMap_Element>);
+	rb_define_alloc_func(rb_cSpriteMap, rb::Alloc<SpriteMapElement>);
 
 	rb_define_method(rb_cSpriteMap, "initialize", _rbf rb_SpriteMap_Initialize, -1);
 	rb_define_method(rb_cSpriteMap, "dispose", _rbf rb_SpriteMap_Dispose, 0);
