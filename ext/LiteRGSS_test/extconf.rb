d2f29a81683eeb657a4e92f03bde8196f6a6b3e1
Scorbutics <brice.bulgarelli@gmail.com>
2020-06-06 22:59:41 +0200

    chore : compilation on Windows for unit tests

--- ext/LiteRGSS_test/extconf.rb
+++ ext/LiteRGSS_test/extconf.rb
@@ -1,16 +1,17 @@
 require_relative 'mkmf_unit_test'
 require 'fileutils'
 
+# add include path to the internal folder
+# $(srcdir) is a root folder, where "extconf.rb" is stored
+$INCFLAGS << " -I/usr/include/LiteCGSS/ -I$(srcdir)/../../ -I$(srcdir)/../../LiteCGSS/"
+$LDFLAGS << " -L$(srcdir)/../../LiteCGSS_bin"
+
 have_library('sfml-graphics')
 have_library('sfml-window')
 have_library('sfml-system')
 
 current_dir_path = File.expand_path(File.dirname(__FILE__)) 
 CONFIG["codedir"] = current_dir_path + '/../LiteRGSS'
-
-# add include path to the internal folder
-# $(srcdir) is a root folder, where "extconf.rb" is stored
-$INCFLAGS << " -I/usr/include/LiteCGSS/"
 
 $CXXFLAGS += " -std=c++17 "
 
