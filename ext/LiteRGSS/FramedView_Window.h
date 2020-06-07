928e212c69fcc03187b48166007f9b9be283a6a9
Scorbutics <brice.bulgarelli@gmail.com>
2020-06-07 12:04:45 +0200

    fix : bad init values for FramedView (Window)

--- ext/LiteRGSS/FramedView_Window.h
+++ ext/LiteRGSS/FramedView_Window.h
@@ -12,26 +12,26 @@ struct FramedViewElement :
     public CgssInstance<cgss::FramedView> {
 
     VALUE rBitmap = Qnil;
-    VALUE rX = Qnil;
-    VALUE rY = Qnil;
-    VALUE rZ = Qnil;
-    VALUE rOX = Qnil;
-    VALUE rOY = Qnil;
+    VALUE rX = LONG2FIX(0);
+    VALUE rY = LONG2FIX(0);
+    VALUE rZ = LONG2FIX(0);
+    VALUE rOX = LONG2FIX(0);
+    VALUE rOY = LONG2FIX(0);
     VALUE rRect = Qnil;
-    VALUE rWidth = Qnil;
-    VALUE rHeight = Qnil;
+    VALUE rWidth = LONG2FIX(1);
+    VALUE rHeight = LONG2FIX(1);
     VALUE rCursorRect = Qnil;
-    VALUE rBackOpacity = Qnil;
-    VALUE rContentOpacity = Qnil;
-    VALUE rOpacity = Qnil;
+    VALUE rBackOpacity = LONG2FIX(255);
+    VALUE rContentOpacity = LONG2FIX(255);
+    VALUE rOpacity = LONG2FIX(255);
     VALUE rWindowBuilder = Qnil;
     VALUE rPauseSkin = Qnil;
     VALUE rCursorSkin = Qnil;
-    VALUE rPause = Qnil;
-    VALUE rPauseX = Qnil;
-    VALUE rPauseY = Qnil;
-    VALUE rActive = Qnil;
-    VALUE rStretch = Qnil;
+    VALUE rPause = Qfalse;
+    VALUE rPauseX = LONG2FIX(0);
+    VALUE rPauseY = LONG2FIX(0);
+    VALUE rActive = Qfalse;
+    VALUE rStretch = Qfalse;
     VALUE rViewport = Qnil;
 
     template <class Drawable, class ... Args>
